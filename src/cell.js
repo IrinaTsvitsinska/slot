a75c63705dd505542bf3006081aa3911732cae79
Volodymyr Liannyi <volodymyr.liannyi@justforthewin.com>
2020-11-05 17:39:06 +0200

    fixinf update;

--- src/cell.js
+++ src/cell.js
@@ -1,53 +1,55 @@
 import * as PIXI from 'pixi.js';
 import { scale } from './animUtils';
-import { setTextureFromUrl } from "./ImageButton";
+import { setTextureFromUrl } from './ImageButton';
 
-// const { TWEEN, Tween, Easing } = require('@tweenjs/tween.js')
-import { Easing, TWEEN } from '@tweenjs/tween.js/dist/tween.umd';
-
+import TWEEN from '@tweenjs/tween.js';
 
 const animationDuration = 3000;
 const maxScale = 2;
 
 export class Cell extends PIXI.Sprite {
-    constructor(x, y, width, height/*, callback*/) {
-        const texture = setTextureFromUrl("images/cells/sym1.png");
-        super(texture);
+  constructor(x, y, width, height /*, callback*/) {
+    const texture = setTextureFromUrl('images/cells/sym1.png');
+    super(texture);
 
-        const graphics = new PIXI.Graphics();
-        graphics.lineStyle(4, 0x00FF7F, 2); // красный DC143C, светлый FFDAB9
-        graphics.drawRect(0, 0, width, height);
+    const graphics = new PIXI.Graphics();
+    graphics.lineStyle(4, 0x00ff7f, 2); // красный DC143C, светлый FFDAB9
+    graphics.drawRect(0, 0, width, height);
 
-        this.position.set(x, y);
-        this.graphics = graphics;
+    this.position.set(x, y);
+    this.graphics = graphics;
 
-        this.addChild(graphics);
-        this.backlightVisible(false);
-        this.interactive = true;
-        this.on('click', () => { this.showActive() });
-    }
+    this.addChild(graphics);
+    this.backlightVisible(false);
+    this.interactive = true;
+    this.on('click', () => {
+      this.showActive();
+    });
+  }
 
-    setTexture(url) {
-        this.texture = setTextureFromUrl(url);
+  setTexture(url) {
+    this.texture = setTextureFromUrl(url);
+  }
 
-    }
+  backlightVisible(ifTrue) {
+    this.graphics.visible = ifTrue;
+  }
 
-    backlightVisible(ifTrue) {
-        this.graphics.visible = ifTrue;
-    }
+  showActive() {
+    console.log('showActive');
+    scale(
+      this,
+      { x: 1, y: 1 },
+      { x: maxScale, y: maxScale },
+      animationDuration / 2,
+      TWEEN.Easing.Quadratic.Out
+    );
+    // .onComplite(
+    //     () => {
 
+    //         scale(this, { x: maxScale, y: maxScale }, { x: 1, y: 1 }, animationDuration / 2/*, Easing.Quadratic.Out*/);
+    //     });
+  }
 
-    showActive() {
-        console.log('showActive');
-        scale(this, { x: 1, y: 1 }, { x: maxScale, y: maxScale }, animationDuration / 2, Easing.Quadratic.Out)
-        // .onComplite(
-        //     () => {
-
-        //         scale(this, { x: maxScale, y: maxScale }, { x: 1, y: 1 }, animationDuration / 2/*, Easing.Quadratic.Out*/);
-        //     });
-    }
-
-    showInActive() {
-
-    }
+  showInActive() {}
 }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,45 @@
 import * as PIXI from 'pixi.js';
 import { SpinButton } from './SpinButton';
 import { GameConfig } from './test';
-import { textureButtons } from "./SpinButton";
+import { textureButtons } from './SpinButton';
 import { Cell } from './cell';
-import { Tween } from '@tweenjs/tween.js';
-import { update } from '@tweenjs/tween.js/dist/tween.umd';
-
-
+import TWEEN from '@tweenjs/tween.js';
 
 const app = new PIXI.Application({
-  width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+  width: 800,
+  height: 600,
+  backgroundColor: 0x1099bb,
+  resolution: window.devicePixelRatio || 1,
 });
 document.body.appendChild(app.view);
 
-// app.ticker.add((/*delta*/) => {
-//   update();
-// });
+app.ticker.add((/*delta*/) => {
+  TWEEN.update();
+});
 
-function animate() {
-  requestAnimationFrame(animate);
-  update();
-}
-requestAnimationFrame(animate);
 // const container = new PIXI.Container();
 
 // app.stage.addChild(container);
 
 // Create a new texture
 
-
 const texture2 = PIXI.Texture.from('images/continue_button_disabled.png');
 const texture1 = PIXI.Texture.from('images/continue_button_normal.png');
 
-const imgBtn = new SpinButton(50, 50, 200, 100, "SpinButton", textureButtons, () => { imgBtn.disable() });
+const imgBtn = new SpinButton(
+  50,
+  50,
+  200,
+  100,
+  'SpinButton',
+  textureButtons,
+  () => {
+    imgBtn.disable();
+  }
+);
 app.stage.addChild(imgBtn);
 
 const cell = new Cell(50, 200, 100, 100);
 app.stage.addChild(cell);
 console.log(imgBtn, cell);
 //imgBtn.position.set(50, 50);
-
-
